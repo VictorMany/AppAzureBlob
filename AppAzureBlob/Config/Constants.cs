b01ca7c2434391193433be101d81f6af6f7821c3
Victor Fuentes <victor.velazfuentez@gmail.com>
2020-05-20 11:41:37 -0500

    Conexiones a almacenamientos de Azure

---
+++ AppAzureBlob/Config/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppAzureBlob.Config
+{
+    public class Constants
+    {
+        public static string StorageConection = "DefaultEndpointsProtocol=https;AccountName=xamformsvictor;AccountKey=Z/FDTNUs7ZR6Y8lYzzhKAce3QgUBZb4pq8Im1Rs5rH40mZbqRu8gEnx2t4ukrR5XN+k8F3sgGTGX3KRwC1gU7A==;EndpointSuffix=core.windows.net";
+    }
+}
---
+++ AppAzureBlob/Services/AzureService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAzureBlob.Services
+{
+    public enum AzureContainer
+    {
+        Image,
+        Text
+    }
+
+    public class AzureService
+    {
+        public static CloudBlobContainer GetContainer(AzureContainer containerType)
+        {
+            var account = CloudStorageAccount.Parse(Config.Constants.StorageConection);
+            //Tener el vinvulo
+            var client = account.CreateCloudBlobClient();
+            return client.GetContainerReference(containerType.ToString().ToLower());
+        }
+
+        //Nos lista todos los archivos de un contenedor
+        public static async Task<IList<string>> GetFilesListAsync(AzureContainer containerType)
+        {
+            //Obtiene todos los archivos blob del contenedor especifico  en un containerType
+            var container = GetContainer(containerType);
+            var list = new List<string>();
+            BlobContinuationToken token = null;
+            do
+            {
+                var result = await container.ListBlobsSegmentedAsync(token);
+                if(result.Results.Count() > 0)
+                {
+                    var blobs = result.Results.Cast<CloudBlockBlob>().Select(b => b.Name);
+                    list.AddRange(blobs);
+                }
+                token = result.ContinuationToken;
+            } while (token != null);
+            return list;
+        }
+
+
+        public static async Task<byte[]> GetFileAsync(AzureContainer containerType, string name)
+        {
+            //Descarga el archivo con el nombre name
+            var container = GetContainer(containerType);
+            var blob = container.GetBlobReference(name);
+            if(await blob.ExistsAsync())
+            {
+                await blob.FetchAttributesAsync();
+                byte[] blobBytes = new byte[blob.Properties.Length];
+                await blob.DownloadToByteArrayAsync(blobBytes, 0);
+                return blobBytes;
+            }
+            return null;
+        }
+
+        public static async Task<string> UploadFileAsync(AzureContainer containerType, Stream stream)
+        {
+            var container = GetContainer(containerType);
+            await container.CreateIfNotExistsAsync();
+
+            //Identificador unico (UUID)
+            var name = Guid.NewGuid().ToString();
+            var fileBlob = container.GetBlockBlobReference(name);
+            await fileBlob.UploadFromStreamAsync(stream);
+
+            return name;
+        }
+
+        public static async Task<bool> DeleteFyleAsync(AzureContainer containerType, string name)
+        {
+            //Borra un archivo blob "name" de un contenedor 
+            var container = GetContainer(containerType);
+            var blob = container.GetBlockBlobReference(name);
+            return await blob.DeleteIfExistsAsync();
+        }
+
+        public static async Task<bool> DeleteContainerAsync(AzureContainer containerType)
+        {
+            //Borra un contenedor 
+            var container = GetContainer(containerType);
+            return await container.DeleteIfExistsAsync();
+        }
+    }
+
+}
+
